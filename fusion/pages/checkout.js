0fc9e8bf23f747acdca48c7c1a4a523e5d71e983
Jepleting <jepletinglinah5@gmail.com>
2023-05-01 15:01:10 +0300

    Displayd product ID in cart

--- fusion/pages/checkout.js
+++ fusion/pages/checkout.js
@@ -1,9 +1,24 @@
 import Layout from "@/components/Layout";
+import { useContext, useEffect } from "react";
+import {ProductsContext} from "@/components/ProductsContext";
+import {useState} from "react";
+
+
 
 export default function checkoutPage(){
+    const {selectedProducts} = useContext(ProductsContext);
+    const [productsInfo,setProductsInfo] = useState ([]);
+    useEffect( () => {
+        const uniqIds = [...new Set(selectedProducts)];
+        fetch ('api/products?ids='+uniqIds.join(','))
+        .then(response => response.json())
+        .then(json => setProductsInfo(json));
+    },[selectedProducts]);
+
+    
     return(
         <Layout>
-            Checkout
+            {selectedProducts.join(',')}
         </Layout>
     );
 }
